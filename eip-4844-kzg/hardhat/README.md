a5bacab9e22403c6053e6f5429e8f3de1f5cb733
Po <chenjunfeng@ethstorage.io>
2024-04-05 21:35:26 -0700

    rename dir

--- eip-4844-kzg/hardhat/README.md
+++ eip-4844-kzg/hardhat/README.md
@@ -1,10 +1,21 @@
-# Sample Hardhat Project
+## kzg-4844 python
+Require python=^3.9
+```
+cd python
+pip install ckzg=1.0.0
+```
+### Ref: 
+- [c-kzg-4844](https://github.com/ethereum/c-kzg-4844/blob/main/bindings/python/tests.py)
+- [js c-kzg](https://github.com/ethers-io/ethers.js/issues/4650#issuecomment-2021007162)
+
+## Hardhat kzg
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
 
 Try running some of the following tasks:
 
 ```shell
+cd hardhat
 npx hardhat help
 npx hardhat test
 REPORT_GAS=true npx hardhat test
--- eip-4844-kzg/hardhat/test/Lock.js
+++ eip-4844-kzg/hardhat/test/Lock.js
@@ -60,7 +60,7 @@ describe("Lock", function () {
       let blobArray = ethers.getBytes(blob);
       expect(blobArray.length).to.equal(FIELD_ELEMENTS_PER_BLOB * 32);
 
-      loadTrustedSetup(path.resolve("/home/po/now/solidity/blob/kzg-4844/trusted_setup.txt"));
+      loadTrustedSetup(path.resolve("../python/trusted_setup.txt"));
       const commitment = blobToKzgCommitment(blobArray);
       console.log("commitment hex:", toHex(commitment));
       const versioned_hash = kzgToVersionedHash(commitment);
@@ -70,7 +70,7 @@ describe("Lock", function () {
       console.log("z bytes32:", ethers.toBeHex(z_int, 32));
       const z = ethers.toBeArray(z_int);
       const [_proof, y] = computeKzgProof(blobArray, z);
-      console.log("proof is:", _proof);
+      console.log("proof is:", toHex(_proof));
       console.log("evaluation is:", y);
 
       let proof = _proof;
@@ -94,70 +94,4 @@ describe("Lock", function () {
 
     });
   });
-
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  // });
 });
